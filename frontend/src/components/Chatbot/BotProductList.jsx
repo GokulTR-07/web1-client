abc3eff962be56e578322322d68ec734785a3568
GokulTR-07 <gokultr1206@gmail.com>
2024-09-09 15:25:16 +0530

    second change

--- frontend/src/components/Chatbot/BotProductList.jsx
+++ frontend/src/components/Chatbot/BotProductList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import BotProductCard from './BotProductCard';
 import { products } from '../../data/Productdata.js';
-import '../../styles/Chatbot.css'
+import './BotProductList.css'
 
 const BotProductList = () => {
   return (
